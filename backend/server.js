a183805d7655ef284f5785e9bbf8ee13a37f10ca
Ruchitakambhampati <ruchitab1237@gmail.com>
2026-03-31 14:32:03 +0530

    Intial commit

--- backend/server.js
+++ backend/server.js
@@ -190,7 +190,8 @@ app.get("/sessions/:userId", async (req, res) => {
     res.status(500).json({ error: "Could not load sessions" });
   }
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
